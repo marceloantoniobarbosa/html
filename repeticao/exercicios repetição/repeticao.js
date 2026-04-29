47907e3966e4d6084358d7379d0512a993aad3c4
Marcelo Antonio Barbosa <marceloantoniobarbosa@gmail.com>
2026-04-29 20:28:00 -0300

    html

--- repeticao/exercicios repetição/repeticao.js
+++ repeticao/exercicios repetição/repeticao.js
@@ -1,5 +1,5 @@
-====================================================================
 /*
+====================================================================
 Basico
 
 // 1. Contagem de 1 a 10:
@@ -22,6 +22,7 @@ if (Number.isNaN(numero)) {
         console.log(`${numero} x ${i} = ${resultado}`);
     }
 }
+
 
 //3. Soma dos primeiros N números naturais:
 
@@ -47,7 +48,52 @@ for (let i = 0; i <= 50; i++) {
     }
 }
 
-*/
-
 // 2. Jogo de adivinhação:
 
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+
+let palpite = 0;
+
+while (palpite !== numeroSecreto) {
+    palpite = Number(prompt("Adivinhe o número (1 a 100):"));
+
+    if (palpite > numeroSecreto) {
+        console.log("O número é menor!");
+    } else if(palpite < numeroSecreto) {
+        console.log("O número é maior!"); 
+    }else {
+        console.log("Acertou!!!");
+    }
+}
+
+// 3. Contagem regressiva:
+
+let numero = Number(prompt("Digite um númeroÇ"));
+
+while (numero >= 0) {
+    console.log(numero);
+
+    numero--;
+}
+
+========================================================================================================
+Nível Avançado
+
+// 01. Soma de 2 digitos
+
+let numero = Number(prompt("Digite um número interiro:"));
+
+let soma = 0;
+
+while (numero > 0) {
+    let digito = numero % 10;
+    soma += digito
+
+    numero = Math.floor(numero/10);
+}
+
+console.log("Soma dos dígitos:", soma);
+
+*/
+
+// 02. Fatorial de um número:
