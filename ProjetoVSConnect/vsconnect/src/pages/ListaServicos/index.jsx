ba6910c65d99da02b11dd847e3319e857fc92618
Marcelo Antonio Barbosa <marceloantoniobarbosa@gmail.com>
2026-06-02 21:54:03 -0300

    html

--- ProjetoVSConnect/vsconnect/src/pages/ListaServicos/index.jsx
+++ ProjetoVSConnect/vsconnect/src/pages/ListaServicos/index.jsx
@@ -1,5 +1,130 @@
-function ListaServicos() {
-    return <h1>Página Lista Servicos</h1>
+import "./style.css";
+
+import Footer2 from "../../components/Footer2";
+
+import cardServico from "../../components/CardServico";
+
+import { useState, useEffect } from "react";
+
+import api from "../../utils/api";
+import CardServico from "../../components/CardServico";
+
+function ListaServicos() {  
+
+    const [listaServicos, setListaServicos] = useState([]);
+
+    const [skillDigitada, setSkillDigitada] = useState("");
+
+    function listarServicos() {
+
+        api.get("servicos")
+
+        .then((response) => {
+
+            setListaServicos(response.data);
+        })
+
+        .catch((error) => {
+
+            console.log("Error, error")
+        });
+    }
+
+
+    function verificarCampoFiltro(event) {
+
+        if (event.target.value =="") {
+            listarServicos();
+        }
+
+        setSkillDigitada(event.target.value);
+    }
+
+    function buscarServicoPorSkill(event) {
+
+        event.preventDefault();
+
+        const servicosFiltrados = listaServicos.filter((servico) => 
+
+            servico.techs.includes(
+                skillDigitada.toLocaleUpperCase()
+            )
+
+        );
+
+        if (servicosFiltrados.length === 0) {
+            alert("Nenhum serviço com essa Skill :(")
+        } else {
+            setListaServicos(servicosFiltrados)
+        }
+    }
+
+    useEffect(() => {
+        listarServicos();
+    }, [])
+
+    return (
+        <div>
+            <main id="ls_main">
+                <div className="container container_lista_servicos">
+
+                    <div className="lista_servico_conteudo">
+
+                        <h1>Lista de Servicos</h1>
+                        <hr />
+
+                        <form method="post" onSubmit={buscarServicoPorSkill}>
+                            <div className="wrapper_form">
+                                <label htmlFor="buscar">
+                                    Filtrar serviços por tecnologia
+                                </label>
+
+                                <div className="campo_label">
+                                    <input type="search" 
+                                            name="campo-busca"
+                                            id="busca"
+                                            placeholder="Buscar serviços por tecnologias...."
+                                            onChange={verificarCampoFiltro}                                 
+                                    />
+
+                                    <button type="submit">
+                                            Buscar
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div className="wrapper_lista">
+
+                            <ul>
+                                {listaServicos.map((servico, indice) =>{
+                                    return (
+                                        <li key={indice}>
+
+                                            <CardServico
+                                            id={servico.id}
+                                            titulo={servico.nome}
+                                            proposta={servico.valor}
+                                            listaTechs={servico.techs}
+                                            descricao={servico.descricao}
+                                            />
+
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </main>
+
+            <Footer2/>
+        </div>
+
+    )
 }
 
 export default ListaServicos;
